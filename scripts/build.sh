58f87ca2bf6ca9eb891e4ff06d8aeb5e8a6b0528
Pete Emerson <pemerson@standard.ai>
2020-06-18 14:14:41 -0700

    build: Added simple build script

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+buildTime=$(date +%s)
+buildDate=$(date -d @${buildTime} -u "+%Y-%m-%d %H:%M:%S UTC")
+buildHash=$(git rev-parse HEAD)
+if [ ! -z "$(git status --porcelain)" ]; then
+        buildHash="$buildHash (with uncommitted changes)"
+fi
+buildVersion="development"
+buildOS=$(uname -s | tr 'A-Z' 'a-z')
+buildInstallMethod=make
+
+mkdir -p bin
+
+for app in $(ls cmd) ; do
+	cd cmd/$app\
+	go build\
+		-a -ldflags "-X 'main.buildDate=${buildDate}'
+		-X 'main.buildHash=${buildHash}'
+		-X 'main.buildVersion=${buildVersion}'
+		-X 'main.buildOS=${buildOS}'
+		-X 'main.buildInstallMethod=${buildInstallMethod}'" \
+		-o ../../bin/$app
+done
